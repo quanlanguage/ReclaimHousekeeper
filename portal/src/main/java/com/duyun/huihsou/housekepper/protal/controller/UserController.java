f15f69c92ddca103ef9676ffcc155b51a0a5d7b1
郝仕兢 <haoshijing@corp.netease.com>
2018-05-08 21:15:33 +0800

    业务代码开发之控制器加实体类

---
+++ portal/src/main/java/com/duyun/huihsou/housekepper/protal/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.duyun.huihsou.housekepper.protal.controller;
+
+import com.duyun.huihsou.housekepper.protal.service.user.UserService;
+import com.duyun.huishou.housekeeper.ApiResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author haoshijing
+ * @version 2018年05月08日 21:13
+ **/
+@RestController
+@RequestMapping("/h5/api/user")
+public class UserController {
+
+    @RequestMapping("/bindPhone")
+    public ApiResponse<Boolean> bindPhone(HttpServletRequest request,String phone){
+        return new ApiResponse<>(false);
+    }
+}
