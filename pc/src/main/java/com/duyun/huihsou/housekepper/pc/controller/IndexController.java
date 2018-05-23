1a0e62ac1566f581a874b56b8e6f45db4b90d8cb
郝仕兢 <haoshijing@corp.netease.com>
2018-05-23 22:31:44 +0800

    增加pc端逻辑

---
+++ pc/src/main/java/com/duyun/huihsou/housekepper/pc/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.duyun.huihsou.housekepper.pc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author haoshijing
+ * @version 2018年05月23日 22:31
+ **/
+
+@RestController
+public class IndexController {
+}
