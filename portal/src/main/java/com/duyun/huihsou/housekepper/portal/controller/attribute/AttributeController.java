e30367e018434fb522db3a3a3a94129439f555fb
Squidyu <1297554122@qq.com>
2018-06-21 00:04:58 +0800

    'Update:属性名和属性值在同一个接口里给出'

--- portal/src/main/java/com/duyun/huihsou/housekepper/portal/controller/attribute/AttributeController.java
+++ portal/src/main/java/com/duyun/huihsou/housekepper/portal/controller/attribute/AttributeController.java
@@ -2,15 +2,19 @@ package com.duyun.huihsou.housekepper.portal.controller.attribute;
 
 import com.duyun.huihsou.housekepper.portal.inteceptor.VisitorAccessible;
 import com.duyun.huihsou.housekepper.portal.service.attribute.AttributeService;
+import com.duyun.huihsou.housekepper.portal.vo.AttributeInfoVO;
 import com.duyun.huishou.housekeeper.ApiResponse;
 import com.duyun.huishou.housekeeper.po.AttributeDetailEntity;
 import com.duyun.huishou.housekeeper.po.AttributeEntity;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,7 +33,18 @@ public class AttributeController {
     @RequestMapping(value = "/list", method = RequestMethod.POST, produces="application/json")
     public ApiResponse<List<AttributeEntity>> getList(@RequestBody Map<String, Object> map) {
         List<AttributeEntity> list = attributeService.getAttributeByCategoryId((Integer) map.get("categoryId"));
-        return new ApiResponse(list);
+        List<AttributeInfoVO> attributeInfoVOList = new ArrayList<>();
+        list.forEach(obj->{
+            AttributeInfoVO attributeInfoVO = new AttributeInfoVO();
+            BeanUtils.copyProperties(obj, attributeInfoVO);
+
+            Map<String, Object> param = new HashMap<>();
+            param.put("attributeId", obj.getId());
+            List<AttributeDetailEntity> attributeDetailEntityList = attributeService.getAttributeDetailByCondetion(param);
+            attributeInfoVO.setAttributeDetailEntityList(attributeDetailEntityList);
+            attributeInfoVOList.add(attributeInfoVO);
+        });
+        return new ApiResponse(attributeInfoVOList);
     }
 
     @VisitorAccessible
---
+++ portal/src/main/java/com/duyun/huihsou/housekepper/portal/vo/AttributeInfoVO.java
@@ -0,0 +1,57 @@
+package com.duyun.huihsou.housekepper.portal.vo;
+
+import com.duyun.huishou.housekeeper.po.AttributeDetailEntity;
+
+import java.util.List;
+
+public class AttributeInfoVO {
+    private Integer id;
+
+    private Integer categoryId;
+
+    private Integer parentId;
+
+    private String attributeName;
+
+    private List<AttributeDetailEntity> attributeDetailEntityList;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    public void setAttributeName(String attributeName) {
+        this.attributeName = attributeName;
+    }
+
+    public List<AttributeDetailEntity> getAttributeDetailEntityList() {
+        return attributeDetailEntityList;
+    }
+
+    public void setAttributeDetailEntityList(List<AttributeDetailEntity> attributeDetailEntityList) {
+        this.attributeDetailEntityList = attributeDetailEntityList;
+    }
+}
