46d7aee693f656e0bff30be8a895f8ed361cc703
quanlanguage@gmail.com <wangquan>
2018-06-26 22:09:44 +0800

    一些业务逻辑上传

---
+++ pc/src/main/java/com/duyun/huihsou/housekepper/pc/service/order/OrderService.java
@@ -0,0 +1,7 @@
+package com.duyun.huihsou.housekepper.pc.service.order;
+
+import com.duyun.huihsou.housekepper.pc.service.IBaseService;
+import com.duyun.huishou.housekeeper.po.OrderEntity;
+
+public interface OrderService extends IBaseService<OrderEntity> {
+}
