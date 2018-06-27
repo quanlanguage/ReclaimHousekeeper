d213672faafa49b4e800df95ee4bd311b5fcedfe
zhongmc <zhongmc_me@163.com>
2018-06-27 23:27:25 +0800

    add jwt

---
+++ pc/src/main/java/com/duyun/huihsou/housekepper/pc/service/UserService.java
@@ -0,0 +1,7 @@
+package com.duyun.huihsou.housekepper.pc.service;
+
+/**
+ * Created by zhongmc on 2018/6/27.
+ */
+public class UserService {
+}
---
+++ pc/src/main/java/com/duyun/huihsou/housekepper/pc/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.duyun.huihsou.housekepper.pc.service.impl;
+
+/**
+ * Created by zhongmc on 2018/6/27.
+ */
+public class UserServiceImpl {
+}
