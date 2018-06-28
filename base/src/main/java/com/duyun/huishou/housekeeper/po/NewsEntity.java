a193396352ca72f2f984244f514d60f6e501081c
丁香尚人_language <quanlanguage@gmail.com>
2018-06-29 00:11:54 +0800

    修正合并分支

--- base/src/main/java/com/duyun/huishou/housekeeper/po/NewsEntity.java
+++ base/src/main/java/com/duyun/huishou/housekeeper/po/NewsEntity.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class NewsEntity {
 
+
     //没有重新构造img 函数
     // 重写构造方法
     public NewsEntity(Integer id,String title,String content,Byte isTop,Long insertTime,Long lastUpdateTime){
@@ -14,6 +15,15 @@ public class NewsEntity {
         setIsTop(isTop);
         setLastUpdateTime(lastUpdateTime);
         setInsertTime(insertTime);
+    }
+
+    //没有重新构造img 函数
+    public NewsEntity(Integer id,String title,String content,Byte isTop,Long lastUpdateTime){
+        setId(id);
+        setContent(content);
+        setTitle(title);
+        setIsTop(isTop);
+        setLastUpdateTime(lastUpdateTime);
     }
 
    
